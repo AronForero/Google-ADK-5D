8e755a420aa1b67e490d5f049b58fda4c7c332fa
Aron Forero <foreroo965@gmail.com>
2025-11-11 15:38:05 -0500

    1 Day: Types of simple and built-in agents with Google ADK

---
+++ loop-agent/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import Agent, SequentialAgent, ParallelAgent, LoopAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.genai import types
+
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
+)
